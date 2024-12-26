c5b20792f1281514387850d0aa9fa6482a06cde1
Irina Gavrilina <irina.gavrilina.08@yandex.ru>
2024-12-26 13:10:40 +0700

    Correct ShareItTests.contextLoads

--- src/test/java/ru/practicum/shareit/ShareItTests.java
+++ src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -1,9 +1,9 @@
 package ru.practicum.shareit;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-@SpringBootTest
+@DataJpaTest
 class ShareItTests {
 
 	@Test
