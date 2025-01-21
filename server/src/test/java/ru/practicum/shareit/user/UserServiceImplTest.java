de70fffe37818a9870c4496624f67fadfc5d0283
Irina Gavrilina <irina.gavrilina.08@yandex.ru>
2025-01-21 19:20:06 +0700

    Correct checkstyle

--- server/src/test/java/ru/practicum/shareit/user/UserServiceImplTest.java
+++ server/src/test/java/ru/practicum/shareit/user/UserServiceImplTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.HttpStatus;
 import ru.practicum.shareit.exception.ErrorHandler;
 import ru.practicum.shareit.exception.ErrorResponse;
 import ru.practicum.shareit.exception.NotFoundException;
